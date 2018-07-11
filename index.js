97dc3a23d9e524a1303065f46a8637d7eeb1ee98
timothy.knoll@gmail.com <timothy.knoll@gmail.com>
2018-07-11 02:41:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function inc (n){
+  n ++ 
+  return n
+}
+
+function dec(n){
+  n-- 
+  return n
+}
+
+function makeInt(n){
+  parseInt(n) 
+  return n
+}
+
+function makeInt(n){
+  parseInt ((n) , 10) 
+  return n
+}
+
+function preserveDecimal(n){
+  Number.parseFloat(n)
+  return n
+}
+
+function preserveDecimal(n){
+  parseFloat('nonsense!')
+  return n
+}
